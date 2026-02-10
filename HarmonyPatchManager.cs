28efd94c72304c41f5e143dc3e58e0733696df0b
zxasv125125 <83066790+zxasv125125@users.noreply.github.com>
2026-02-10 20:33:02 +0700

    Update HarmonyPatchManager.cs

--- HarmonyPatchManager.cs
+++ HarmonyPatchManager.cs
@@ -10,7 +10,7 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.GameData;
-using StardewValley.GameData.Objects;;
+using StardewValley.GameData.Objects;
 
 namespace EasterEgg
 {
