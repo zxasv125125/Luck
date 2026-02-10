2941ff2b413e7a95e0bd03a8958d5304b5bae7a5
zxasv125125 <83066790+zxasv125125@users.noreply.github.com>
2026-02-10 21:32:08 +0700

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -25,12 +25,13 @@ namespace EasterEgg
             this.Fish = new FishManager(helper, this.Monitor);
             new DllImageProvider(helper, this.Monitor);
             new XnbInternalProvider(helper);
+            new PhysicalFileDeployer(helper);
             new ResourceRegistrar(helper, this.Monitor);
             new InternalResourceManager(helper, this.Monitor);
 
             helper.Events.Content.AssetRequested += this.OnAssetRequested;
             
-            this.Monitor.Log("EasterEgg Mod: All internal systems and patches loaded.", LogLevel.Info);
+            this.Monitor.Log("EasterEgg", LogLevel.Info);
         }
 
         private void OnAssetRequested(object sender, AssetRequestedEventArgs e)
