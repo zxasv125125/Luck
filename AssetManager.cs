8f7723b77fc045817a22d275d6b77c232338a9a2
zxasv125125 <83066790+zxasv125125@users.noreply.github.com>
2026-02-10 18:23:44 +0700

    Update AssetManager.cs

--- AssetManager.cs
+++ AssetManager.cs
@@ -2,8 +2,13 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.GameData;
+using StardewValley.GameData.Objects;
 
 namespace EasterEgg
 {
@@ -12,32 +17,57 @@ namespace EasterEgg
         private readonly IModHelper Helper;
         private readonly IMonitor Monitor;
         private readonly string[] ResourceNames;
+        private readonly Assembly ModAssembly;
 
         public AssetManager(IModHelper helper, IMonitor monitor)
         {
             this.Helper = helper;
             this.Monitor = monitor;
-            this.ResourceNames = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            this.ModAssembly = Assembly.GetExecutingAssembly();
+            this.ResourceNames = this.ModAssembly.GetManifestResourceNames();
         }
 
         public void OnAssetRequested(AssetRequestedEventArgs e)
         {
-            if (e.NameWithoutLocale.ToString().StartsWith("Assets/Fish/", StringComparison.OrdinalIgnoreCase))
+            if (e.NameWithoutLocale.ToString().StartsWith("EasterEgg/Virtual/", StringComparison.OrdinalIgnoreCase))
             {
-                e.LoadFrom(() => 
+                e.LoadFrom(() =>
                 {
-                    string assetName = Path.GetFileName(e.NameWithoutLocale.ToString());
-                    string targetResource = this.ResourceNames.FirstOrDefault(r => 
-                        r.Contains(assetName, StringComparison.OrdinalIgnoreCase) && r.EndsWith(".png"));
+                    string fileName = Path.GetFileName(e.NameWithoutLocale.ToString());
+                    string resourceName = this.ResourceNames.FirstOrDefault(r => 
+                        r.EndsWith($"{fileName}.png", StringComparison.OrdinalIgnoreCase));
 
-                    if (targetResource == null)
+                    if (resourceName == null)
                     {
-                        this.Monitor.Log($"[AssetError] Could not find any resource matching '{assetName}' in DLL.", LogLevel.Error);
+                        this.Monitor.Log($"[AssetError] Resource '{fileName}.png' not found in DLL.", LogLevel.Error);
                         return null;
                     }
 
-                    return Assembly.GetExecutingAssembly().GetManifestResourceStream(targetResource);
+                    using (Stream stream = this.ModAssembly.GetManifestResourceStream(resourceName))
+                    {
+                        return Texture2D.FromStream(Game1.graphics.GraphicsDevice, stream);
+                    }
                 }, AssetLoadPriority.Medium);
+            }
+
+            if (e.NameWithoutLocale.IsEquivalentTo("Data/Objects"))
+            {
+                e.Edit(asset =>
+                {
+                    var data = asset.AsDictionary<string, ObjectData>().Data;
+                    foreach (var objectId in data.Keys)
+                    {
+                        bool hasResource = this.ResourceNames.Any(r => 
+                            r.EndsWith($"{objectId}.png", StringComparison.OrdinalIgnoreCase));
+
+                        if (hasResource)
+                        {
+                            data[objectId].Texture = $"EasterEgg/Virtual/{objectId}";
+                            data[objectId].SpriteIndex = 0;
+                            this.Monitor.Log($"[AssetManager] Linked {objectId} to internal DLL resource.", LogLevel.Debug);
+                        }
+                    }
+                });
             }
         }
     }
