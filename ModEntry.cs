d6028d733ae31da4757b0e3d8e57d240590b2e1b
zxasv125125 <83066790+zxasv125125@users.noreply.github.com>
2026-02-10 20:02:06 +0700

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -3,6 +3,9 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Collections.Generic;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
@@ -20,7 +23,17 @@ namespace EasterEgg
         {
             this.Assets = new AssetManager(helper, this.Monitor);
             this.Fish = new FishManager(helper, this.Monitor);
+            new DllImageProvider(helper, this.Monitor);
+            new ResourceRegistrar(helper, this.Monitor);
+            new ResourceRedirector(helper, this.Monitor);
+            new InternalImageLoader(helper, this.Monitor);
+            new InternalResourceManager(helper, this.Monitor);
+            new HarmonyPatchManager(this.ModManifest.UniqueID, this.Monitor);
+            new HarmonyResourcePatcher(this.ModManifest.UniqueID, this.Monitor);
+
             helper.Events.Content.AssetRequested += this.OnAssetRequested;
+            
+            this.Monitor.Log("EasterEgg Mod: All internal systems and patches loaded.", LogLevel.Info);
         }
 
         private void OnAssetRequested(object sender, AssetRequestedEventArgs e)
