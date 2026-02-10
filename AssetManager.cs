33a80e27c1735c05b6bb0258d3885acc05c029a2
zxasv125125 <83066790+zxasv125125@users.noreply.github.com>
2026-02-10 17:39:38 +0700

    Update AssetManager.cs

--- AssetManager.cs
+++ AssetManager.cs
@@ -1,18 +1,17 @@
-using System;
-using System.IO;
-using System.Reflection;
+using System.Collections.Generic;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.GameData.Objects;
 
 namespace EasterEgg
 {
-    public class AssetManager
+    public class FishManager
     {
-        private readonly IModHelper Helper;
-        private readonly IMonitor Monitor;
-        private readonly string RootPath = "EasterEgg.Assets.Fish";
+        private IModHelper Helper;
+        private IMonitor Monitor;
 
-        public AssetManager(IModHelper helper, IMonitor monitor)
+        public FishManager(IModHelper helper, IMonitor monitor)
         {
             this.Helper = helper;
             this.Monitor = monitor;
@@ -20,23 +19,44 @@ namespace EasterEgg
 
         public void OnAssetRequested(AssetRequestedEventArgs e)
         {
-            if (e.NameWithoutLocale.StartsWith("Assets/Fish/", StringComparison.OrdinalIgnoreCase))
+            if (e.NameWithoutLocale.IsEquivalentTo("Data/Objects"))
             {
-                e.LoadFrom(() => 
-                {
-                    string assetName = Path.GetFileName(e.NameWithoutLocale.ToString());
-                    string resourcePath = $"{this.RootPath}.{assetName.ToLower()}.png";
-                    var assembly = Assembly.GetExecutingAssembly();
-                    var stream = assembly.GetManifestResourceStream(resourcePath);
-                    if (stream == null)
-                    {
-                        this.Monitor.Log($"[AssetError] not found '{resourcePath}' in DLL", LogLevel.Warn);
-                        return null;
-                    }
-
-                    return stream;
-                }, AssetLoadPriority.Medium);
+                e.Edit(asset => {
+                    var data = asset.AsDictionary<string, ObjectData>().Data;
+                    this.AddFishObject(data, "EasterEgg_Degend", "Degend", "The Luck Species but it seems to be marines animal.", 30000, 0, true, "degend");
+                });
             }
+            if (e.NameWithoutLocale.IsEquivalentTo("Data/Fish"))
+            {
+                e.Edit(asset => {
+                    var data = asset.AsDictionary<string, string>().Data;
+                    data["EasterEgg_Degend"] = "Degend/110/sinker/40/60/600 2000/winter/rainy/688 .05/10/0/.1/10/false";
+                });
+            }
+        }
+
+        private void AddFishObject(IDictionary<string, ObjectData> data, string id, string name, string description, int price, int index, bool isLegendary, string textureName)
+        {
+            var contextTags = new List<string>();
+            if (isLegendary)
+            {
+                contextTags.Add("fish_legendary");
+                contextTags.Add("item_legendary");
+            }
+
+            data[id] = new ObjectData
+            {
+                Name = id,
+                DisplayName = name,
+                Description = description,
+                Type = "Fish",
+                Category = -4,
+                Price = price,
+                Texture = $"Assets/Fish/{textureName}",
+                SpriteIndex = index,
+                ContextTags = contextTags,
+                ExcludeFromFishingCollection = false 
+            };
         }
     }
 }
