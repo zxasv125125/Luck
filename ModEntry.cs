796de1de77d80068aaf1f8238b69f4c335c5a1d8
zxasv125125 <83066790+zxasv125125@users.noreply.github.com>
2026-02-10 08:38:38 +0700

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -7,7 +7,6 @@ namespace EasterEgg
     {
         public AssetManager Assets;
         public FishManager Fish;
-        public AnimalManager Animals;
 
         public override void Entry(IModHelper helper)
         {
@@ -21,23 +20,7 @@ namespace EasterEgg
         {
             this.Assets.OnAssetRequested(e);
             this.Fish.OnAssetRequested(e);
-            this.Animals.OnAssetRequested(e);
         }
-        public void OnUpdateTicked(UpdateTickedEventArgs e)
-        {
-        if (!Context.IsWorldReady) return;
-        foreach (FarmAnimal animal in Game1.getFarm().animals.Values)
-        {
-        if (animal.type.Value == "Custom_SuperCow")
-        {
-            if (animal.Sprite.CurrentAnimation == null)
-            {
-                animal.Sprite.currentFrame = 20; 
-            }
-        }
-    }
-}
-
     }
 }
 
